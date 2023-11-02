4feb682dfff0212ebef486fe2d7fb5c917c92057
Lucas Araújo <araujolucas@dcc.ufmg.br>
2023-11-02 13:19:09 -0300

    + Update clang-format file + Indent code according new clang-format file + Add vector construtor with default value to initialize elements in the   array + Translate comments to english in vector class

--- include/vector.h
+++ include/vector.h
@@ -1,119 +1,142 @@
 /*
-* Filename: vector.h
-* Created on: June  5, 2023
-* Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
-*/
+ * Filename: vector.h
+ * Created on: June  5, 2023
+ * Author: Lucas Araújo <araujolucas@dcc.ufmg.br>
+ */
 
 #ifndef VECTOR_H_
 #define VECTOR_H_
 
-#include "utils.h"
-#include "vector_excpt.h"
 #include <cstddef>
 #include <initializer_list>
 
-#define VECTOR_START_SIZE 8
-// The growth factor determines how much a vector should grow when it needs to be resized
+#include "utils.h"
+#include "vector_excpt.h"
+
+// The growth factor determines how much a vector should grow when it needs to
+// be resized
 #define VECTOR_GROWTH_FACTOR 2
+#define VECTOR_START_SIZE 8
 
-
-// TODO implementar funções: insert(pos, valor), erase(pos)
-
+/**
+ * @brief A dynamic array-based vector class
+ *
+ * This class provides dynamic array functionality for storing and managing elements
+ * of type 'typeT'. It allows for insertion, deletion, and various operations on
+ * the elements in the vector
+ *
+ * @tparam typeT The type of elements stored in the vector
+ *
+ * TODO implement functions: insert(pos, value), erase(pos)
+ */
 template<typename typeT>
-class Vector {
+class Vector
+{
     private:
-        typeT *m_elements; // Pointer to first element of vector (array)
-        std::size_t m_capacity; // Total space used by vector, including elements and free space
-        std::size_t m_size; // Num of elements in vector
+        // Pointer to first element of vector (array)
+        typeT* m_elements;
+        // Total space used by vector, including elements and free space
+        std::size_t m_capacity;
+        // Num of elements in vector
+        std::size_t m_size;
 
     public:
         /**
-        @brief Construtor padrão da classe
-        */
+         * @brief Default constructor
+         */
         Vector();
 
         /**
-        @brief Construtor
-        @param size Espaço inicial alocado para o vector
-        */
+         * @brief Constructor
+         * @param size Initial space allocated for the vector
+         */
         Vector(std::size_t size);
 
         /**
-         * @brief Construtor with initializer list to receive data as {x1, x2, x3, ..., xn}
+         * @brief Constructor
+         * @param size Initial space allocated for the vector
+         * @param value Initialization value for the elements in the vector
+         */
+        Vector(std::size_t size, typeT value);
+
+        /**
+         * @brief Construtor with initializer list to receive data as {x1, x2, x3,
+         *..., xn}
          **/
         Vector(const std::initializer_list<typeT> values);
 
         /**
-        @brief Destrutor da classe
-        */
+         * @brief Destructor
+         */
         ~Vector();
 
         /**
          * @brief Copy constructor
          **/
-        Vector(const Vector<typeT> &other);
+        Vector(const Vector<typeT>& other);
 
         /**
          * @brief Assignment operator
          **/
-        Vector &operator=(const Vector<typeT> &other);
+        Vector& operator=(const Vector<typeT>& other);
 
         /**
-        @brief Overload do operador []
-        @param index Índice do elemento que será buscado
-        @return Elemento na posição index
-        @throw InvalidIndex Caso o índice seja inválido
-        */
-        typeT &operator[](std::size_t index);
-        typeT &operator[](std::size_t index) const;
+         * @brief Overload do operador []
+         * @param index Índice do elemento que será buscado
+         * @return Elemento na posição index
+         * @throw InvalidIndex Caso o índice seja inválido
+         */
+        typeT& operator[](std::size_t index);
+        typeT& operator[](std::size_t index) const;
 
         /**
-        @brief Overload do operador ==
-        @param other Vetor que será usado na verificação
-        @return True se forem iguais, False caso contrário
-        */
-        bool operator==(Vector<typeT> &other);
+         * @brief Operator overload for ==
+         * @param other Vector to be used for comparison
+         * @return True if they are equal, False otherwise
+         */
+        bool operator==(Vector<typeT>& other);
 
         /**
-        @brief Pega o tamanho atual do vector
-        @return Inteiro que representa o tamanho do vector
-        */
+         * @brief Get the current size of the vector
+         * @return An integer representing the size of the vector
+         */
         std::size_t Size();
         std::size_t Size() const;
 
         /**
-        @brief Pega o tamanho máximo atual (quando esse limite for alcança o vector é realocado para comportar mais elementos)
-        @return Inteiro que representa o tamanho máximo atual do vector
-        */
+         * @brief Get the current maximum size (when this limit is reached, the vector
+         * is reallocated to accommodate more elements)
+         * @return An integer representing the current maximum size of the vector
+         */
         std::size_t GetMaxSize();
 
         /**
-        @brief Verifica se o vector está vazio
-        @return True se o vector estiver vazio, False caso contrário
-        */
+         * @brief Check if the vector is empty
+         * @return True if the vector is empty, False otherwise
+         */
         bool IsEmpty();
 
         /**
-        @brief Troca a posição de dois elementos
-        @param index1, index2 Posições dos elementos que serão trocados de posição
-        @throw InvalidIndex Caso algum dos índice seja inválido
-        */
+         * @brief Swap the positions of two elements
+         * @param index1, index2 Positions of the elements to be swapped
+         * @throw InvalidIndex If any of the indices is invalid
+         */
         void Swap(std::size_t index1, std::size_t index2);
 
         /**
-        @brief Insere um novo elemento no fim do vector
-        @param element Novo elemento
-        */
+         * @brief Insert a new element at the end of the vector
+         * @param element New element
+         */
         void PushBack(typeT element);
 
         /**
-        @brief Remove o elemento que está no fim do vector
-        */
+         * @brief Remove the element at the end of the vector
+         */
         void PopBack();
 
         /**
-        @brief Limpa o vector
-        */
+         * @brief Clear the vector
+         */
         void Clear();
 
         /**
@@ -124,7 +147,7 @@ class Vector {
         void Resize(std::size_t newSize, typeT val = typeT());
 
         /**
-         * @brief Allocate a new space for this vector
+         * * @brief Allocate a new space for this vector
          * @param newalloc Size of the new allocate
          **/
         void Reserve(std::size_t newalloc);
@@ -132,43 +155,44 @@ class Vector {
         /**
          * @return The element at the specified index
          **/
-        typeT &At(std::size_t index);
+        typeT& At(std::size_t index);
 
-        // Implementação do iterator
+        // Iterator
         using value_type = typeT;
-        using pointer = typeT *;
-        using reference = typeT &;
+        using pointer    = typeT*;
+        using reference  = typeT&;
 
-        typedef struct Iterator {
+        typedef struct Iterator
+        {
             public:
-                Iterator(pointer ptr) : m_ptr(ptr) { }
+                Iterator(pointer ptr) : m_ptr(ptr) {}
 
-                Iterator() { }
+                Iterator() {}
 
-                reference operator*() const {
-                    return *m_ptr;
-                }
+                reference operator*() const { return *m_ptr; }
 
-                pointer operator->() {
-                    return m_ptr;
-                }
+                pointer operator->() { return m_ptr; }
 
-                Iterator &operator++() {
+                Iterator& operator++()
+                {
                     m_ptr++;
                     return *this;
                 }
 
-                Iterator operator++(int) {
+                Iterator operator++(int)
+                {
                     Iterator tmp = *this;
                     ++(*this);
                     return tmp;
                 }
 
-                friend bool operator==(const Iterator &a, const Iterator &b) {
+                friend bool operator==(const Iterator& a, const Iterator& b)
+                {
                     return a.m_ptr == b.m_ptr;
                 };
 
-                friend bool operator!=(const Iterator &a, const Iterator &b) {
+                friend bool operator!=(const Iterator& a, const Iterator& b)
+                {
                     return a.m_ptr != b.m_ptr;
                 };
 
@@ -177,27 +201,38 @@ class Vector {
 
         } Iterator;
 
-    Iterator begin() {
-        return Iterator(&m_elements[0]);
-    }
+        Iterator begin() { return Iterator(&m_elements[0]); }
 
-    Iterator end() {
-        return Iterator(&m_elements[m_size]);
-    }
+        Iterator end() { return Iterator(&m_elements[m_size]); }
 };
 
 template<typename typeT>
-Vector<typeT>::Vector() {
+Vector<typeT>::Vector()
+{
     this->m_capacity = VECTOR_START_SIZE;
-    this->m_size = 0;
+    this->m_size     = 0;
     this->m_elements = new typeT[this->m_capacity];
 }
 
 template<typename typeT>
-Vector<typeT>::Vector(std::size_t size) {
+Vector<typeT>::Vector(std::size_t size)
+{
     this->m_capacity = size;
-    this->m_size = 0;
+    this->m_size     = 0;
     this->m_elements = new typeT[this->m_capacity];
+}
+
+template<typename typeT>
+Vector<typeT>::Vector(std::size_t size, typeT val)
+{
+    this->m_capacity = size;
+    this->m_size     = 0;
+    this->m_elements = new typeT[this->m_capacity];
+
+    for (std::size_t i = 0; i < size; i++)
+    {
+        this->m_elements[i] = val;
+    }
 }
 
 template<typename typeT>
@@ -205,20 +240,22 @@ Vector<typeT>::Vector(std::initializer_list<typeT> values)
 {
     this->m_elements = new typeT[values.size()];
     this->m_capacity = values.size();
-    this->m_size = values.size();
+    this->m_size     = values.size();
 
     std::copy(values.begin(), values.end(), m_elements);
 }
 
 template<typename typeT>
-Vector<typeT>::~Vector() {
+Vector<typeT>::~Vector()
+{
     delete[] this->m_elements;
 }
 
 template<typename typeT>
-Vector<typeT>::Vector(const Vector<typeT> &other) {
+Vector<typeT>::Vector(const Vector<typeT>& other)
+{
     this->m_capacity = other.m_capacity;
-    this->m_size = other.m_size;
+    this->m_size     = other.m_size;
     this->m_elements = new typeT[this->m_capacity];
 
     for (std::size_t i = 0; i < this->m_size; i++)
@@ -226,40 +263,45 @@ Vector<typeT>::Vector(const Vector<typeT> &other) {
 }
 
 template<typename typeT>
-Vector<typeT> &Vector<typeT>::operator=(const Vector<typeT> &other) {
+Vector<typeT>& Vector<typeT>::operator=(const Vector<typeT>& other)
+{
     if (this == &other)
         return *this;
 
     delete[] this->m_elements;
 
     this->m_capacity = other.m_capacity;
-    this->m_size = other.m_size;
+    this->m_size     = other.m_size;
     this->m_elements = new typeT[this->m_capacity];
 
     for (std::size_t i = 0; i < this->m_size; i++)
         this->m_elements[i] = other.m_elements[i];
 
-
     return *this;
 }
 
 template<typename typeT>
-typeT &Vector<typeT>::operator[](std::size_t index) {
+typeT& Vector<typeT>::operator[](std::size_t index)
+{
     return this->m_elements[index];
 }
 
 template<typename typeT>
-typeT &Vector<typeT>::operator[](std::size_t index) const {
+typeT& Vector<typeT>::operator[](std::size_t index) const
+{
     return this->m_elements[index];
 }
 
 template<typename typeT>
-bool Vector<typeT>::operator==(Vector<typeT> &other) {
+bool Vector<typeT>::operator==(Vector<typeT>& other)
+{
     if (this->m_size != other.Size())
         return false;
 
-    for (std::size_t i = 0; i < this->m_size; i++) {
-        if (this->m_elements[i] != other[i]) {
+    for (std::size_t i = 0; i < this->m_size; i++)
+    {
+        if (this->m_elements[i] != other[i])
+        {
             return false;
         }
     }
@@ -268,38 +310,46 @@ bool Vector<typeT>::operator==(Vector<typeT> &other) {
 }
 
 template<typename typeT>
-std::size_t Vector<typeT>::Size() {
+std::size_t Vector<typeT>::Size()
+{
     return this->m_size;
 }
 
 template<typename typeT>
-std::size_t Vector<typeT>::Size() const {
+std::size_t Vector<typeT>::Size() const
+{
     return this->m_size;
 }
 
 template<typename typeT>
-std::size_t Vector<typeT>::GetMaxSize() {
+std::size_t Vector<typeT>::GetMaxSize()
+{
     return this->m_capacity;
 }
 
 template<typename typeT>
-bool Vector<typeT>::IsEmpty() {
+bool Vector<typeT>::IsEmpty()
+{
     return this->m_size == 0;
 }
 
 template<typename typeT>
-void Vector<typeT>::Swap(std::size_t index1, std::size_t index2) {
+void Vector<typeT>::Swap(std::size_t index1, std::size_t index2)
+{
     if ((std::size_t)utils::Max(index1, index2) > this->m_capacity)
         throw vecexcpt::InvalidIndex();
 
     typeT aux = this->m_elements[index1];
-    this->m_elements[index1] = this->m_elements[index2];
+
     this->m_elements[index2] = aux;
+    this->m_elements[index1] = this->m_elements[index2];
 }
 
 template<typename typeT>
-void Vector<typeT>::PushBack(typeT element) {
-    if (this->m_size == this->m_capacity) {
+void Vector<typeT>::PushBack(typeT element)
+{
+    if (this->m_size == this->m_capacity)
+    {
         this->Reserve(this->m_capacity * VECTOR_GROWTH_FACTOR);
     }
 
@@ -307,38 +357,45 @@ void Vector<typeT>::PushBack(typeT element) {
 }
 
 template<typename typeT>
-void Vector<typeT>::PopBack() {
-    if (not this->IsEmpty()) {
+void Vector<typeT>::PopBack()
+{
+    if (not this->IsEmpty())
+    {
         this->m_size--;
     }
 }
 
 template<typename typeT>
-void Vector<typeT>::Clear() {
+void Vector<typeT>::Clear()
+{
     this->m_size = 0;
 }
 
 template<typename typeT>
-void Vector<typeT>::Resize(std::size_t newSize, typeT val) {
+void Vector<typeT>::Resize(std::size_t newSize, typeT val)
+{
     this->Reserve(newSize);
 
-    for (std::size_t i = this->m_size; i < newSize; ++i) {
-        this->m_elements[i] = typeT();
+    for (std::size_t i = this->m_size; i < newSize; ++i)
+    {
+        this->m_elements[i] = val;
     }
 
     this->m_size = newSize;
 }
 
 template<typename typeT>
-void Vector<typeT>::Reserve(std::size_t newAlloc) {
+void Vector<typeT>::Reserve(std::size_t newAlloc)
+{
 
     if (newAlloc <= this->m_capacity)
         return;
 
-    typeT *newElements = new typeT[newAlloc]();
+    typeT* newElements = new typeT[newAlloc]();
 
     std::size_t i;
-    for (i = 0; i < this->m_size; i++) {
+    for (i = 0; i < this->m_size; i++)
+    {
         newElements[i] = this->m_elements[i];
     }
 
@@ -348,7 +405,8 @@ void Vector<typeT>::Reserve(std::size_t newAlloc) {
 }
 
 template<typename typeT>
-typeT &Vector<typeT>::At(std::size_t index) {
+typeT& Vector<typeT>::At(std::size_t index)
+{
     if (index > this->m_size)
         throw vecexcpt::InvalidIndex();
 
